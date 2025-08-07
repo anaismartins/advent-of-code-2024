2366b29d1399543d6d7f8e2a2683e96cf72b09b1
Ana Martins <a.i.s.martins@astro.uio.no>
2025-08-07 14:47:42 +0200

    day2 part 1

--- src/day2.f90
+++ src/day2.f90
@@ -4,18 +4,35 @@ program day2
 
    type(IntArray), allocatable :: data(:)
    integer :: i, j, nsafe, diff, temp
+   logical :: safe
 
    data = read_file('/mn/stornext/u3/aimartin/d5/advent-of-code-2024/input/day2.txt', ' ')
 
    nsafe = 0
    temp = 0
+   safe = .true.
    do i = 1, size(data)
       do j = 2, size(data(i)%values)
          diff = data(i)%values(j) - data(i)%values(j-1)
-         if (diff > 3 .or. diff < -3 .or. diff == 0) exit
-         if ((temp > 0 .and. diff < 0) .or. (temp < 0 .and. diff > 0)) exit
+         if (abs(diff) > 3 .or. diff == 0) then
+            safe = .false.
+            print *, 'Unsafe report (condition 1) at line', i, 'column', j, ':', data(i)%values(j-1), '->', data(i)%values(j)
+            exit
+         end if
+
+         if ((temp > 0 .and. diff < 0) .or. (temp < 0 .and. diff > 0)) then
+            safe = .false.
+            print *, 'Unsafe report (condition 2) at line', i, 'column', j, ':', temp, '->', diff
+            print *, 'Values in line:', data(i)%values
+            exit
+         end if
+
          temp = diff
       end do
+      if (safe) nsafe = nsafe + 1
 
+      safe = .true.
+      temp = 0
    end do
+   print *, 'Number of safe reports:', nsafe
 end program day2
